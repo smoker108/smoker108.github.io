bcf477199041d7ada2b15f8d9f00edc6bf1fb380
smoker108 <156664011+smoker108@users.noreply.github.com>
2024-03-19 03:29:46 +0530

    Add files via upload

---
+++ o/main_files/firebase.js
@@ -0,0 +1,14 @@
+     // Your Firebase configuration
+   const firebaseConfig = {
+  apiKey: "AIzaSyCtsBFsSd96BcFwjKvsmlIXzBM3Au3nMr4",
+  authDomain: "dtdtax-44e0e.firebaseapp.com",
+  databaseURL: "https://dtdtax-44e0e-default-rtdb.firebaseio.com",
+  projectId: "dtdtax-44e0e",
+  storageBucket: "dtdtax-44e0e.appspot.com",
+  messagingSenderId: "746360693176",
+  appId: "1:746360693176:web:b939553b3755419f3caf4d"
+};
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    
